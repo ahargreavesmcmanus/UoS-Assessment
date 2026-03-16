ed24a3c7e4e55def6073d4af3e60324a9b64d37e
Alastair Hargreaves-McManus <2028319+ahargreavesmcmanus@users.noreply.github.com>
2026-03-16 16:55:26 +0000

    ETL doc comments

--- etl script.py
+++ etl script.py
@@ -3,13 +3,19 @@
 import datetime
 import os
 
+from pandas import DataFrame
+
 import db
 import pandas as pd
 
 OUTPUT_DIRECTORY = "output"
 
 
-def extract():
+def extract() -> tuple[list[str], list]:
+    """
+    Extract the data from the database
+    :return: column names, data rows from database
+    """
     with db.cnx() as cnx, cnx.cursor() as cursor:
         cnx.database = db.SCHEMA
         cursor.execute(
@@ -34,7 +40,12 @@ def extract():
         return cursor.column_names, cursor.fetchall()
 
 
-def transform(columns: list[str], rows: list):
+def transform(columns: list[str], rows: list) -> DataFrame:
+    """
+    Transform the data into a DataFrame with a concatenated name field and a calculated value field
+    :param columns: column names
+    :param rows: data rows
+    """
     df = pd.DataFrame(columns=columns, data=rows)
     df["name"] = df.apply(lambda row: row["firstname"] + " " + row["surname"], axis=1)
     df["value"] = df.apply(lambda row: row["unit_price"] * row["quantity"] if pd.notna(row["quantity"]) else None,
@@ -42,10 +53,14 @@ def transform(columns: list[str], rows: list):
     return df
 
 
-def load(df):
+def load(df: DataFrame) -> None:
+    """
+    Outputs the DataFrame as a CSV with a timestamp filename to the output directory
+    :param df:
+    """
     filename = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S") + ".csv"
     filepath = os.path.join(os.path.dirname(__file__), OUTPUT_DIRECTORY, filename)
-    with open(filepath, "xb") as file:
+    with open(filepath, "xb") as file:  # "b" open type to avoid double processing by pandas
         df.to_csv(file, index=False)
     print("Saved to", filepath)
 
@@ -54,7 +69,6 @@ def main():
     columns, rows = extract()
     df = transform(columns, rows)
     load(df)
-
 
 if __name__ == "__main__":
     main()
