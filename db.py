881ceb0c2bf4ada8c1ecdfc43f012efaf418d855
Alastair Hargreaves-McManus <2028319+ahargreavesmcmanus@users.noreply.github.com>
2026-03-16 16:40:53 +0000

    db.py get functions for DB parameters

--- db.py
+++ db.py
@@ -7,10 +7,21 @@ dotenv.load_dotenv()
 
 SCHEMA = os.environ.get("SCHEMA_NAME")
 
+def get_user() -> str:
+    return os.environ.get("DB_USER")
+def get_password() -> str:
+    return os.environ.get("DB_PASS")
+def get_host() -> str:
+    return os.environ.get("DB_HOST")
+def get_port() -> int:
+    return int(os.environ.get("DB_PORT"))
+
+
 def cnx():
     return mysql.connector.connect(
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASS"),
-        host=os.environ.get("DB_HOST"),
+        user=get_user(),
+        password=get_password(),
+        host=get_host(),
+        port=get_port(),
         consume_results=True
     )
